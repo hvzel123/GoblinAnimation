81b2a0a558f18946a044c50972cc77727210be5f
Glenn Pak <glennpak159874@gmail.com>
2022-01-18 20:01:26 -0800

    Goblin

---
+++ goblinRight.js
@@ -0,0 +1,19 @@
+class goblinRight {
+	constructor(game) {
+		this.game = game;
+		this.animator = new Animator(ASSET_MANAGER.getAsset("./goblinSpriteRight.png"), 0, 0, 64, 57, 7, 0.2);
+		
+		this.x = 525;
+		this.y = 5;
+		this.speed = 50;
+	};
+
+	update() {
+		
+	};
+
+	draw(ctx) {
+		this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+
+	};
+}
